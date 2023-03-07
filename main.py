887cf17205eabd4cf0ee5bbfe6c16db66ed03f95
SnoozingSimian <ayangofficial@gmail.com>
2023-03-07 09:43:50 +0530

    update to current telegram version

--- main.py
+++ main.py
@@ -1,88 +1,86 @@
-import os
 import logging
+import os
+import sys
 
-# util imports
-from utils.parse_transaction import parse_transaction
 from utils.delete_transaction import remove_last
+from utils.parse_transaction import parse_transaction
 from utils.store_transaction import store_transaction
 
 from dotenv import load_dotenv
 
-# telegram imports
-from telegram.ext import Updater
 from telegram import Update
-from telegram.ext import CallbackContext
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler, Filters
-
-
-# version
-VERSION = "0.0.1"
+from telegram import __version__ as TG_VER
+from telegram.ext import (
+    filters,
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    MessageHandler,
+)
 
 # securely get the API_KEY
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
-# set logging on
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 
-# connect bot
-#bot = telegram.Bot(token=API_KEY)
+# version
+APP_VER = "0.0.2"
+PY_VER = sys.version
 
-# create updater
-updater = Updater(token=API_KEY, use_context=True)
-dispatcher = updater.dispatcher
+logging.info(f"PYTHON: {PY_VER}\nTELEGRAM: {TG_VER}\nAPPLICATION VER: {APP_VER}")
 
-# define commands here
 
-# start message
-def start(update: Update, context: CallbackContext):
-	context.bot.send_message(chat_id=update.effective_chat.id, 
-				 text=f"This is leger_bot version {VERSION}\nI hope I can be of use")
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"This is leger_bot version {APP_VER}\nI hope I can be of use",
+    )
+
+
+async def transaction(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    transaction = parse_transaction(update)
+    store = store_transaction(transaction)
+
+    if store == 1:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id, text="Thanks for telling me!"
+        )
+    elif store == 0:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="I could not understand that, the format is <item> <price>",
+        )
 
 
 # add support for deleting last entry
-def delete_last(update: Update, context: CallbackContext):
-	transaction = parse_transaction(update)
-	remove = remove_last(transaction)
-	
-	if remove == 1:
-		context.bot.send_message(chat_id=update.effective_chat.id,
-				 text="Removed last entry.")
-	
-	elif remove == 0:
-		context.bot.send_message(chat_id=update.effective_chat.id,
-				 text="Could not remove last entry.")
+async def delete_last(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    transaction = parse_transaction(update)
+    remove = remove_last(transaction)
 
-def summary():
-	#stub
-    return 
+    if remove == 1:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id, text="Removed last entry."
+        )
+
+    elif remove == 0:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id, text="Could not remove last entry."
+        )
 
 
-# define message handling here
-# our bot needs to simply parse input text into items and costs
-# for now, the input format should be <item> <cost>
-def transaction(update: Update, context: CallbackContext):
-	transaction = parse_transaction(update)
-	store = store_transaction(transaction)
-	
-	if store == 1:
-		context.bot.send_message(chat_id=update.effective_chat.id,
-				 	 text="Thanks for telling me!")
+if __name__ == "__main__":
+    application = ApplicationBuilder().token(API_KEY).build()
 
-	elif store == 0:
-		context.bot.send_message(chat_id=update.effective_chat.id,
-				 	 text="I could not understand that, the format is <item> <price>")
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
 
+    transaction_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), transaction)
+    application.add_handler(transaction_handler)
 
-# handlers handle different types of actions by user
-start_handler = CommandHandler('start', start)
-delete_last_handler = CommandHandler('rml', delete_last)
-transaction_handler = MessageHandler(Filters.text & (~Filters.command), transaction)
+    remove_handler = CommandHandler("remove", delete_last)
+    application.add_handler(remove_handler)
 
-# the above handlers need to be addded to the dispatcher
-dispatcher.add_handler(start_handler)
-dispatcher.add_handler(transaction_handler)
-dispatcher.add_handler(delete_last_handler)
-updater.start_polling()
+    application.run_polling()
